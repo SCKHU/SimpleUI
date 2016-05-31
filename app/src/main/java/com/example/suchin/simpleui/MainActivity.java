611823ab7bf651be8437a255124516d4c50c5470
SCKHU <chiusuchin@gmail.com>
2016-05-31 13:23:03 +0800

    2016.05.30 class 1

--- app/src/main/java/com/example/suchin/simpleui/MainActivity.java
+++ app/src/main/java/com/example/suchin/simpleui/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.suchin.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Debug", "Hello log");
+        textview = (TextView)findViewById(R.id.textview);// the function findViewById output Class  is View, need to downcasting to TextView.
+        textview.setText("Hello TextView");
     }
 }
