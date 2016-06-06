e66c00a60451460ee985309f42de2836aa5ae85d
SCKHU <chiusuchin@gmail.com>
2016-06-06 19:54:02 +0800

    list view

--- app/src/main/java/com/example/suchin/simpleui/MainActivity.java
+++ app/src/main/java/com/example/suchin/simpleui/MainActivity.java
@@ -5,21 +5,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     TextView textview;
     EditText editview;
     RadioGroup radiogroup;
-    String gender = "Female";
     CheckBox checkbox;
-    String name = "";
-    String HideGender = "";
-
+    //    String gender = "Female";
+//    String name = "";
+//    String HideGender = "";
+    String drinkname = "Black Tea";
+    ListView listView;
+    ArrayList<String> drinks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,58 +42,73 @@ public class MainActivity extends AppCompatActivity {
         radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
         checkbox = (CheckBox) findViewById(R.id.checkBox);
         //2016.06.02 Anonymouse Function
-        editview.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)//when enter pressed
-                {
-                    click(v);
-
-                    return true; //block the "Enter" key, or the textUI would increase one line
-                }
-                return false;
-            }
-        });
+        listView = (ListView) findViewById(R.id.listView);
+//        setupListView();
+//        editview.setOnKeyListener(new View.OnKeyListener() {
+//            @Override
+//            public boolean onKey(View v, int keyCode, KeyEvent event) {
+//                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)//when enter pressed
+//                {
+//                    click(v);
+//
+//                    return true; //block the "Enter" key, or the textUI would increase one line
+//                }
+//                return false;
+//            }
+//        });
 
         radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.MaleRadioButton) {
-                    gender = "Male";
-                } else if (checkedId == R.id.FemaleRadioButton) {
-                    gender = "Female";
-                }
+//                if (checkedId == R.id.MaleRadioButton) {
+//                    gender = "Male";
+//                } else if (checkedId == R.id.FemaleRadioButton) {
+//                    gender = "Female";
+//                }
+                RadioButton radiobutton = (RadioButton) findViewById(checkedId);
+                drinkname = radiobutton.getText().toString();
             }
         });
 
-        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                changeTextView();
-            }
-        });
+
+//        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                changeTextView();
+//            }
+//        });
+//    }
     }
 
     public void click(View view) // here class buttonview also valid
     {
-        name = editview.getText().toString();
-        changeTextView();
-        editview.setText("");
+        String note = editview.getText().toString();
+        //textview.setText(drinkname);
+        drinks.add(drinkname);
+        setupListView();
+//        changeTextView();
+//        editview.setText("");
     }
 
-    public void changeTextView() {
-        if (name.equals(""))
-            return;
-        if (checkbox.isChecked()) {
+//    public void changeTextView() {
+//        if (name.equals(""))
+//            return;
+//        if (checkbox.isChecked()) {
+//
+//
+//            textview.setText(name);
+//        } else {
+//
+//            String content = "Name: "+name + ", Gender: " + gender;
+//            textview.setText(content);
+//        }
+//    }
 
-
-            textview.setText(name);
-        } else {
-
-            String content = "Name: "+name + ", Gender: " + gender;
-            textview.setText(content);
-        }
-
-
+    void setupListView() {
+//        String[] data = new String[]{"123", "Helllo", "234", "Hi", "ABCD"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drinks);
+        listView.setAdapter(adapter);
     }
+
+
 }
